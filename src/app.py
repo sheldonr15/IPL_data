f4acaba9838fef6742f122ac1bfa2f29a603393a
sheldonr15 <sheldonrodrigues150@gmail.com>
2020-04-25 18:58:17 +0530

    files for 'compare wins' and 'stadium win with bat / field first'

--- src/app.py
+++ src/app.py
@@ -27,6 +27,51 @@ def most_home_wins(df, year: int):
         fig = px.bar(df_plot, x="winner", y="TF", hover_data=["TF"], labels={"winner": "Team", "TF":"Number of wins at homeground"})
         plot(fig)
 
+def compare_wins(option1, option2):
+    def wps(year):
+        df_year = df["winner"].loc[df["season"]==year].value_counts().rename_axis('teams').reset_index(name='wins')
+        df_year["season"] = year
+        return df_year
+
+    frames = [None]*12
+    for i in range(2008, 2020):
+        frames[i-2008] = wps(i)
+
+    df_plot = pd.concat(frames)
+    df_plot2 = df_plot.loc[df_plot["teams"].isin([option1, option2])]
+    fig = px.line(df_plot2, x="season", y="wins", color="teams")
+    plot(fig)
+
+def bat_field(decision):
+    df["bat/field"] = "NO"
+    df.loc[(df["toss_winner"] == df["winner"]) & (df["toss_decision"] == "bat"), "bat/field"] = "bat"
+    df.loc[(df["toss_winner"] == df["winner"]) & (df["toss_decision"] == "field"), "bat/field"] = "field"
+    df.loc[(df["bat/field"] == "NO") & (df["toss_decision"] == "field"), "bat/field"] = "bat"
+    df.loc[(df["bat/field"] == "NO") & (df["toss_decision"] == "bat"), "bat/field"] = "field"
+    
+    df_batfield = df.groupby(["venue", "bat/field"]).size().reset_index(name='counts')
+    df_total_matches = df_batfield.groupby(["venue"])["counts"].sum().reset_index(name="total_matches")
+    df_batfield2 = df_batfield.copy()
+    stad_matches = dict(zip(df_total_matches.venue, df_total_matches.total_matches))
+    for index, row in df_batfield2.iterrows():
+        for stadium in stad_matches:
+            if row.venue == stadium:
+                df_batfield2.at[index, "total_m"] = int(stad_matches[stadium])
+
+    df_batfield2["total_m"] = df_batfield2["total_m"].astype(int)
+    df_batfield2["perc_of_wins"] = round((df_batfield2["counts"]/df_batfield2["total_m"])*100,2)
+        
+    if decision == "all":
+        fig  = px.bar(df_batfield2, y = "venue", x="perc_of_wins", orientation="h", color="bat/field", height=1000, labels={"venue":"Stadiums", "perc_of_wins":"Win percentage"})
+        
+    else:
+        sorted_bf = df_batfield2.loc[df_batfield2["bat/field"]==str(decision)][:11].sort_values(by="perc_of_wins", ascending=True)
+        fig  = px.bar(sorted_bf, y = "venue", x="perc_of_wins", orientation="h", opacity=0.7, labels={"venue":"Stadiums", "perc_of_wins":f"Win percentage with {decision} first"}, width=1000)
+        
+
+    plot(fig)
+
+
 
 def main():
     st.sidebar.title("MENU")
@@ -43,3 +88,26 @@ def main():
 
         st.header("Most Homeground Wins in all seasons ( 2008 - 2019 )")
         most_home_wins(df, 0)
+
+    compare_val = st.sidebar.checkbox("Compare Wins Per Team")
+    if compare_val is True:
+        option1 = st.selectbox("Select Team 1", pd.unique(df["team1"]))
+        option2 = st.selectbox("Select Team 2", pd.unique(df["team1"].loc[df["team1"]!=option1]))
+        st.subheader(f"Comparing Wins per Season between {option1} and {option2}")
+        compare_wins(option1, option2)
+
+    batandfield_val = st.sidebar.checkbox("Stadiums preferring Bat first or Field first")
+    if batandfield_val is True:
+        st.subheader("Win Percentage for Stadiums when batting / fielding first")
+        bat_field("all")
+
+        st.subheader("Stats of stadiums with ")
+        select = ["Bat First", "Field First"]
+        select_option = st.selectbox("Select Bat or Field", select)
+        if select_option is "Only Bat First":
+            batandfield_option = "bat"
+        else:
+            batandfield_option = "field"
+        
+        bat_field(batandfield_option)
+
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,14 +1,26 @@
-from .sheets import data, headers
+# from .sheets import data, headers
 # import sheets
 import pandas as pd
+from datetime import datetime
 
 def load_dataset():
-    df = pd.DataFrame(data, columns=headers)
+    # df = pd.DataFrame(data, columns=headers)
+    df = pd.read_csv("data/matches.csv")
+    df["date"] = pd.to_datetime(df["date"])
+    df["city"] = df["city"].fillna("Dubai")
     df["team1"] = df["team1"].replace("Rising Pune Supergiants", "Rising Pune Supergiant")
     df["team2"] = df["team2"].replace("Rising Pune Supergiants", "Rising Pune Supergiant")
     df["winner"] = df["winner"].replace("Rising Pune Supergiants", "Rising Pune Supergiant")
     df["city"] = df["city"].replace("Bangalore", "Bengaluru")
-    df["city"].dropna(inplace=True)
+    # df["city"].dropna(inplace=True)
+    df["venue"] = df["venue"].replace("Rajiv Gandhi International Stadium, Uppal", "Rajiv Gandhi Intl. Cricket Stadium")
+    df["venue"] = df["venue"].replace("Punjab Cricket Association IS Bindra Stadium, Mohali", "Punjab Cricket Association Stadium")
+    df["venue"] = df["venue"].replace("Punjab Cricket Association Stadium, Mohali", "Punjab Cricket Association Stadium")
+    df["venue"] = df["venue"].replace("M Chinnaswamy Stadium", "M. Chinnaswamy Stadium")
+    df["venue"] = df["venue"].replace("Feroz Shah Kotla", "Feroz Shah Kotla Ground")
+    df["venue"] = df["venue"].replace("MA Chidambaram Stadium, Chepauk", "M. A. Chidambaram Stadium")
+    df["venue"] = df["venue"].replace("IS Bindra Stadium", "Punjab Cricket Association Stadium")
+    df["venue"] = df["venue"].replace("ACA-VDCA Stadium", "Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium")
 
     team_city = {
         'Sunrisers Hyderabad': ['Hyderabad', 'Visakhapatnam'],
