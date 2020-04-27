3b1680790c96ee88e8e75b96bdf2cf54d5f82618
sheldonr15 <sheldonrodrigues150@gmail.com>
2020-04-27 18:02:26 +0530

    heroku deploy:

---
+++ notebooks/sheets.py
@@ -0,0 +1,19 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets",
+"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("../creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("matches").sheet1
+
+data = sheet.get_all_records()
+headers = data.pop(0)
+# print(data[0])
+
+# sheetsdataframe = pd.DataFrame(data, columns=headers)
+# print(df.head())
